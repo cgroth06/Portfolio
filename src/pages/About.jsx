b5fa7b5fed8ff2c6f9fb7cb8c57a02f37e9ea2c7
cgroth06 <christopher.m.groth@gmail.com>
2024-12-16 12:27:04 -0600

    about update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,8 +8,7 @@ export default function About() {
         <p className = "p">
           Chris Groth is a full stack developer with a passion for problem solving, learning, 
           and collaborating. Chris has a background in the arts and education, and is excited 
-          to bring his unique perspective to the world of web development. Chris is currently a student 
-          of the University of Minnesota Web Development Bootcamp, and is eager to continue 
+          to bring his unique perspective to the world of web development. Chris recently completed the University of Minnesota Full Stack Flex Web Development Bootcamp, and is eager to continue 
           learning and growing in the field of web development.
         </p>
       </div>
